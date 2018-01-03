9672b3e01f60b6dab6b206e3abef5a6aa3f7b617
George Baron <gbaron1@sheffield.ac.uk>
2018-01-03 12:59:51 +0000

    Adds a new loading icon

--- app/src/main/java/com4510/thebestphotogallery/Activities/LoadingActivity.java
+++ app/src/main/java/com4510/thebestphotogallery/Activities/LoadingActivity.java
@@ -1,20 +1,9 @@
 package com4510.thebestphotogallery.Activities;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com4510.thebestphotogallery.Database.AppDatabase;
-import com4510.thebestphotogallery.Database.DatabaseResponseListener;
-import com4510.thebestphotogallery.Database.ImageMetadata;
-import com4510.thebestphotogallery.Images.LoadImagesResponseListener;
 import com4510.thebestphotogallery.R;
-import com4510.thebestphotogallery.Tasks.LoadImagesTask;
 import com4510.thebestphotogallery.Tasks.ReadFromDatabaseTask;
 import com4510.thebestphotogallery.Util;
 
