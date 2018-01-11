0311f475b0e6e8d9bd5c419d2b2a8b6a13de13e7
Joshua O'Leary <jmoey139@gmail.com>
2018-01-11 13:17:39 +0000

    Get basic filter select interaction working

---
+++ app/src/main/java/com4510/thebestphotogallery/Activities/FilterSelectActivity.java
@@ -0,0 +1,107 @@
+package com4510.thebestphotogallery.Activities;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import com4510.thebestphotogallery.R;
+
+public class FilterSelectActivity extends AppCompatActivity {
+
+    private Calendar startDate = Calendar.getInstance();
+    private Calendar endDate = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_select);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final TextInputEditText startDateEntry = findViewById(R.id.startdate_entry);
+        TextInputEditText endDateEntry = findViewById(R.id.enddate_entry);
+
+        startDateEntry.setKeyListener(null);
+        endDateEntry.setKeyListener(null);
+
+//        final Activity that = this;
+//
+//        startDateEntry.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(that, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//
+//                    }
+//                }, 0,0, 0);
+//                datePickerDialog.show();
+//            }
+//        });
+//
+//        startDateEntry.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if (hasFocus) startDateEntry.performClick();
+//            }
+//        });
+
+        // set start date to yesterday's date
+        startDate.add(Calendar.DATE, -1);
+
+        setupDatePicker(startDateEntry, startDate);
+        setupDatePicker(endDateEntry, endDate);
+
+//        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+//
+//        startDateEntry.setText(dateFormat.format(yesterday.getTime()));
+//        endDateEntry.setText(dateFormat.format(today.getTime()));
+    }
+
+    private void setupDatePicker(final TextInputEditText textInput, final Calendar date) {
+        final Activity that = this;
+        final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        textInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(that, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.set(year, month, dayOfMonth);
+                        textInput.setText(dateFormat.format(date.getTime()));
+                    }
+                }, date.get(Calendar.YEAR),date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
+            }
+        });
+
+        textInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) textInput.performClick();
+            }
+        });
+
+
+        textInput.setText(dateFormat.format(date.getTime()));
+    }
+
+
+}
--- app/src/main/java/com4510/thebestphotogallery/Activities/MainActivity.java
+++ app/src/main/java/com4510/thebestphotogallery/Activities/MainActivity.java
@@ -3,6 +3,9 @@ package com4510.thebestphotogallery.Activities;
 import android.app.Activity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
@@ -12,20 +15,24 @@ import android.os.Environment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.util.Log;
 import android.net.Uri;
 import android.support.v4.content.FileProvider;
+import android.widget.DatePicker;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
 import java.io.File;
 import java.util.List;
+import java.util.TimeZone;
 
 import com4510.thebestphotogallery.Database.AppDatabase;
 import com4510.thebestphotogallery.Database.ImageMetadata;
@@ -53,6 +60,10 @@ public class MainActivity extends ImageLoadActivity {
     private boolean permissionsOk = true;
 
     private String mCurrentPhotoPath = null;
+
+    int filterYear = 0;
+    int filterMonth = 0;
+    int filterDay = 0;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -235,6 +246,65 @@ public class MainActivity extends ImageLoadActivity {
 
                 startActivity(intent);
                 break;
+            case R.id.btn_filter_date:
+                intent = new Intent(this, FilterSelectActivity.class);
+                startActivity(intent);
+
+//                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                        setFilterDate(year, month, dayOfMonth);
+//                    }
+//                };
+
+//                Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+//
+
+//                DatePickerDialog dialog = new DatePickerDialog(this, dateSetListener,
+//                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+//
+//                DatePicker dp1 = new DatePicker(this);
+//                DatePicker dp2 = new DatePicker(this);
+//
+//                final AlertDialog date2Dialog = new AlertDialog.Builder(this)
+//                        .setTitle("End date")
+//                        .setView(dp2)
+//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//
+//                            }
+//                        })
+//                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                finish();
+//                            }
+//                        })
+//                        .create();
+//
+//
+//                final AlertDialog date1Dialog = new AlertDialog.Builder(this)
+//                        .setTitle("Start date")
+//                        .setView(dp1)
+//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                date2Dialog.show();
+//                                finish();
+//
+//                            }
+//                        })
+//                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                finish();
+//                            }
+//                        })
+//                        .create();
+//
+//                date1Dialog.show();
+
         }
         return true;
     }
@@ -285,6 +355,12 @@ public class MainActivity extends ImageLoadActivity {
         queueRefresh = false;
         recyclerViewAdapter.clear();
         super.refresh();
+    }
+
+    private void setFilterDate(int year, int month, int day) {
+        filterYear = year;
+        filterMonth = month;
+        filterDay = day;
     }
 
 }
