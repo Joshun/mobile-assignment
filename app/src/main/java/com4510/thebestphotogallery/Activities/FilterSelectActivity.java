622597c45ed07b81a27f740b557e04d5368fb15d
Joshua O'Leary <jmoey139@gmail.com>
2018-01-11 13:39:05 +0000

    Check that end date is after start date and add proper title

--- app/src/main/java/com4510/thebestphotogallery/Activities/FilterSelectActivity.java
+++ app/src/main/java/com4510/thebestphotogallery/Activities/FilterSelectActivity.java
@@ -1,14 +1,19 @@
 package com4510.thebestphotogallery.Activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Filter;
 import android.widget.TextView;
@@ -31,10 +36,17 @@ public class FilterSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_select);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("Filter by Date");
+        }
+        else {
+            Log.w(getClass().getName(), "toolbar is null");
+        }
 
-        final TextInputEditText startDateEntry = findViewById(R.id.startdate_entry);
+        TextInputEditText startDateEntry = findViewById(R.id.startdate_entry);
         TextInputEditText endDateEntry = findViewById(R.id.enddate_entry);
+        Button filterActionBtn = findViewById(R.id.filter_action_btn);
 
         startDateEntry.setKeyListener(null);
         endDateEntry.setKeyListener(null);
@@ -46,6 +58,32 @@ public class FilterSelectActivity extends AppCompatActivity {
         setupDatePicker(startDateEntry, startDate);
         setupDatePicker(endDateEntry, endDate);
 
+        final Activity activity = this;
+
+        filterActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (endDate.before(startDate)) {
+                    AlertDialog dialog = new AlertDialog.Builder(activity)
+                            .setTitle("Cannot have end before start!")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            })
+                            .create();
+                    dialog.show();
+                }
+                else {
+                    Intent intent = new Intent();
+                    intent.putExtra("startDate", startDate);
+                    intent.putExtra("endDate", endDate);
+                    setResult(RESULT_OK);
+                    finish();
+                }
+            }
+        });
     }
 
     private void setupDatePicker(final TextInputEditText textInput, final Calendar date) {
